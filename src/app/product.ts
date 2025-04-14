ba001413fb3a4e47d322d7909d1981f65e1becdd
Ahmed-Elimam <aa.emam97@gmail.com>
2025-04-15 01:58:02 +0200

    product component

---
+++ src/app/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id: number;
+    productName: string;
+    productPrice: number;
+    productImage: string;
+    onSale: boolean;
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-
+  products: Product[] =[
+    {
+      id: 1,
+      productName: 'Whey Protein',
+      productPrice: 49.99,
+      productImage: 'assets/products/protein.jpg',
+      onSale: true
+    },
+    {
+      id: 2,
+      productName: 'Creatine Monohydrate',
+      productPrice: 29.99,
+      productImage: 'assets/products/creatine.png',
+      onSale: false
+    },
+    {
+      id: 3,
+      productName: 'Shaker Bottle',
+      productPrice: 12.99,
+      productImage: 'assets/products/shaker.png',
+      onSale: true
+    }
+  ]
 }

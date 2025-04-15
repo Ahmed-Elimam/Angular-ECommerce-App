39b5aba445d7415c45bd13499d28ff658efa2590
Ahmed-Elimam <aa.emam97@gmail.com>
2025-04-15 02:19:36 +0200

    category component

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Category } from '../category'; 
 @Component({
   selector: 'app-category',
   imports: [],
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './category.component.css'
 })
 export class CategoryComponent {
-
+  categories: Category[] =[
+    {
+        id: 1,
+        title: 'Supplements',
+        price: 150,
+        image: 'assets/Categories/supplements.webp'
+    },
+    {
+        id: 2,
+        title: 'Clothing',
+        price: 50,
+        image: 'assets/Categories/clothing.png'
+    },
+    {
+        id: 3,
+        title: 'Equipment',
+        price: 200,
+        image: 'assets/Categories/equipment.png'
+    },
+  ]
 }

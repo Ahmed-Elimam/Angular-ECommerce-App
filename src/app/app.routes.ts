529e2c2587f68a80767052999bd2f0070f6454f4
Ahmed-Elimam <aa.emam97@gmail.com>
2025-04-17 02:18:48 +0200

    routing for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'products', loadComponent: () => import('./products-list/products-list.component').then(m => m.ProductsListComponent)},
+    
+    
+    
+    
+    {path: '**',component: NotFoundComponent}
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../navbar/navbar.component';
 import { ProductComponent } from '../product/product.component';
 import { CategoryComponent } from '../category/category.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent,ProductComponent,CategoryComponent,FooterComponent],
+  imports: [ProductComponent,CategoryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
